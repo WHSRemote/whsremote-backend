870116d696f26aa1e6cf034ed212a622fc7f1a52
Mingle Li <limingle5@gmail.com>
2020-09-22 13:46:57 -0400

    Added special schedule support properly

--- src/main/java/com/whsremote/api/controller/ScheduleController.java
+++ src/main/java/com/whsremote/api/controller/ScheduleController.java
@@ -11,8 +11,8 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 @Controller
@@ -67,21 +67,46 @@ public class ScheduleController {
         String body = "{}";
 
         try {
-            File myObj = new File("schedule_update.txt");
-            Scanner myReader = new Scanner(myObj);
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                if (!data.equals("{}")) {
-                    body = data;
+            InputStream is = getFileFromResourceAsStream("schedule_update.txt");
+            if (is == null) {
+                return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+
+            InputStreamReader streamReader =
+                    new InputStreamReader(is, StandardCharsets.UTF_8);
+            BufferedReader reader = new BufferedReader(streamReader);
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (!line.equals("{}")) {
+                    body = line;
                 }
             }
-            myReader.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("An error occurred.");
+
+        } catch (IOException e) {
             e.printStackTrace();
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
         return new ResponseEntity(body, HttpStatus.OK);
     }
+
+    // get a file from the resources folder
+    // works everywhere, IDEA, unit test and JAR file.
+    private InputStream getFileFromResourceAsStream(String fileName) {
+
+        // The class loader that loaded the class
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream(fileName);
+
+        // the stream holding the file content
+        if (inputStream == null) {
+            System.err.println("File not found!");
+            return null;
+        } else {
+            return inputStream;
+        }
+
+    }
+
 
 }
